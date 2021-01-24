3e460d34cfe028ea2cb3cbcd73af02155ff92d2e
Kunal <kunalgupta8122@gmail.com>
2021-01-24 18:02:53 +0530

    Oldest Website Created By Me

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let lol = "baklol";
+console.log("My String is " + (typeof lol));
+let piper = 8787.32;
+console.log("My String is " + (typeof piper));
+
+function findName(){
+    
+}
+console.log( typeof findName);
